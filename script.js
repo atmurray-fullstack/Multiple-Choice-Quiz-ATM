784a7b42c11fc6aa7280681baf11f2486df1d100
Andrew Murray <atmurray@bellsouth.net>
2020-03-19 13:44:19 -0400

    added player initial form and js variable arrays

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+//immutable variables
+
+//question array
+const qAll =[
+    ['question1', [answer1-1,true], [answer1-2,false], [answer1-3,false],[anser1-4, false]],
+    ['question2', [answer2-1,true], [answer2-2,false], [answer2-3,false],[anser2-4, false]],
+    ['question3', [answer3-1,true], [answer3-2,false], [answer3-3,false],[anser3-4, false]],
+    ['question4', [answer4-1,true], [answer4-2,false], [answer4-3,false],[anser4-4, false]],
+    ['question5', [answer5-1,true], [answer5-2,false], [answer5-3,false],[anser5-4, false]]
+
+]
+// answer value array
+var playerAns=[]||playerAns;
+//array for player name
+playerName = [];
+//time counter 
+counter = 60;
