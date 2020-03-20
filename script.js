fbbf5c69d80057cb51398f731d00b6289de70425
Andrew Murray <atmurray@bellsouth.net>
2020-03-20 11:49:59 -0400

    created event listeners for buttons and set element vars

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Element variables
+var h1Elem = document.querySelector('h1');
+var a1 = document.getElementById('answer-1');
+var a2 = document.getElementById('answer-2');
+var a3 = document.getElementById('answer-3');
+var a4 = document.getElementById('answer-4');
+//answers form
+var ansForm = document.getElementById('answers');
+var valid = document.getElementById('valid');
+
 
 
 //immutable variables
@@ -13,41 +23,150 @@ const qAll = [
 ]
 // answer value array
 var playerAns = [] || playerAns;
+
 //array for player name
 var playerName = [];
+
 //time counter 
 var counter = 60;
+
 //random question array choose variable
 var randomQ = 0;
+
+
 //confirm ready variable
 var ready = confirm('Are you ready?');
+
 //array to make sure questions are not repeated
 var oldQ = [];
 
 
+
+
+
+
+
+
 //setInterval variable to make timer and display in <p> in 
-var playTime = setInterval(function () {
-    counter--;
-    console.log(counter)
-    document.getElementById('countDown').innerHTML = 'TIME REMAINING: '+counter;
-    if (counter == 0) {
-        clearInterval(playTime)
-        alert('Times is up! Let\'s checkout your score')
-    }
-}, 100);
+// var playTime = setInterval(function () {
+//     counter--;
+//     console.log(counter)
+//     document.getElementById('countDown').innerHTML = 'TIME REMAINING: ' + counter;
+//     if (counter == 0) {
+//         clearInterval(playTime)
+//         alert('Times is up! Let\'s checkout your score')
+//     }
+// }, 100);
+
+if (ready) {
+
+    randomQ = Math.floor(Math.random() * qAll.length);
+
+    //set button value equal to answers
+    a1.value = qAll[randomQ][1][0];
+    a2.value = qAll[randomQ][2][0];
+    a3.value = qAll[randomQ][3][0];
+    a4.value = qAll[randomQ][4][0];
+
+    //set h1 innerText equal to question
+    h1Elem.innerText = qAll[randomQ][0];
+
+}
+
+//Event listener for answer button 1
+
+a1.addEventListener('click', function () {
+    console.log(a1.value);
+    playerAns.push(qAll[randomQ][1][1]);
+    console.log(qAll[randomQ][1][1])
+    console.log(playerAns)
+
+if(qAll[randomQ][1][1]){
+    valid.innerText = 'Correct Answer';
+}else {
+    valid.innerText = 'Incorrect Answer';
+}
 
 
-randomQ = Math.floor(Math.random()*qAll.length);
-
-//set button value equal to answers
-document.getElementById('question-1').value=
-qAll[randomQ][1][0];
-
-//set h1 innerText equal to question
-document.querySelector('h1').innerText = qAll[randomQ][0];
-
-//console.log(randomQ)
+    randomQ = Math.floor(Math.random() * qAll.length)
+    h1Elem.innerText = qAll[randomQ][0];
+    a1.value = qAll[randomQ][1][0];
+    a2.value = qAll[randomQ][2][0];
+    a3.value = qAll[randomQ][3][0];
+    a4.value = qAll[randomQ][4][0];
 
 
+})
+
+//event listeners for answer button 2
+a2.addEventListener('click', function () {
+    console.log(a2.value);
+    playerAns.push(qAll[randomQ][2][1]);
+    console.log(qAll[randomQ][2][1])
+    console.log(playerAns)
+
+if(qAll[randomQ][2][1]){
+    valid.innerText = 'Correct Answer';
+}else {
+    valid.innerText = 'Incorrect Answer';
+}
 
 
+    randomQ = Math.floor(Math.random() * qAll.length)
+    h1Elem.innerText = qAll[randomQ][0];
+    a1.value = qAll[randomQ][1][0];
+    a2.value = qAll[randomQ][2][0];
+    a3.value = qAll[randomQ][3][0];
+    a4.value = qAll[randomQ][4][0];
+
+
+})
+
+
+//event listener for button 3
+a3.addEventListener('click', function () {
+    console.log(a3.value);
+    playerAns.push(qAll[randomQ][3][1]);
+    console.log(qAll[randomQ][3][1])
+    console.log(playerAns)
+
+if(qAll[randomQ][3][1]){
+    valid.innerText = 'Correct Answer';
+}else {
+    valid.innerText = 'Incorrect Answer';
+}
+
+
+    randomQ = Math.floor(Math.random() * qAll.length)
+    h1Elem.innerText = qAll[randomQ][0];
+    a1.value = qAll[randomQ][1][0];
+    a2.value = qAll[randomQ][2][0];
+    a3.value = qAll[randomQ][3][0];
+    a4.value = qAll[randomQ][4][0];
+
+
+})
+
+//event listener for button 4
+a4.addEventListener('click', function () {
+    console.log(a4.value);
+    playerAns.push(qAll[randomQ][4][1]);
+    console.log(qAll[randomQ][4][1])
+    console.log(playerAns)
+
+if(qAll[randomQ][4][1]){
+    valid.innerText = 'Correct Answer';
+}else {
+    valid.innerText = 'Incorrect Answer';
+}
+
+
+    randomQ = Math.floor(Math.random() * qAll.length)
+    h1Elem.innerText = qAll[randomQ][0];
+    a1.value = qAll[randomQ][1][0];
+    a2.value = qAll[randomQ][2][0];
+    a3.value = qAll[randomQ][3][0];
+    a4.value = qAll[randomQ][4][0];
+
+
+})
