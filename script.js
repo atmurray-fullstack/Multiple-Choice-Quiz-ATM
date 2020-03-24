174bc6675cad82aacbcf3b4e9bdc106fb8aaa3e8
Andrew Murray <atmurray@bellsouth.net>
2020-03-24 10:39:36 -0400

    cleanupt

--- script.js
+++ script.js
@@ -26,23 +26,25 @@ const qAll = [
     ['question11', ['answer11-1', true], ['answer11-2', false], ['answer11-3', false], ['answer11-4', false]],
     ['question12', ['answer12-1', true], ['answer12-2', false], ['answer12-3', false], ['answer12-4', false]],
     ['question13', ['answer13-1', true], ['answer13-2', false], ['answer13-3', false], ['answer13-4', false]]
-   
+
 
 ]
 // answer value array
 var playerAns = [] || playerAns;
-
 //array for player name
 var playerName = [];
-
 //time counter 
 var counter = 60;
-
 //random question array choose variable
 var randomQ = 0;
-
 var ready = false;
+var score = 0;
+scoreDis.innerText = 'Score: ' + score
+//array to make sure questions are not repeated
+var oldQ = [];
 
+//create functions/////////
+////////////////////////
 function rando() {
     return Math.floor(Math.random() * qAll.length);
 }
@@ -59,42 +61,28 @@ function countDown() {
     }, 1000);
 }
 
-function questionsSet(){
+function questionsSet() {
     if (ready) {
-
         randomQ = rando();
-    
         //set button value equal to answers
         a1.value = qAll[randomQ][1][0];
         a2.value = qAll[randomQ][2][0];
         a3.value = qAll[randomQ][3][0];
         a4.value = qAll[randomQ][4][0];
-    
         //set h1 innerText equal to question
         h1Elem.innerText = qAll[randomQ][0];
-    
+
     }
-    
-    
 }
-
-
-
-
-
-var score = 0;
-scoreDis.innerText = 'Score: ' + score
-
-//array to make sure questions are not repeated
-var oldQ = [];
-
+//////////////////////////
+//////////////////////////
 
 //Initial h1 game statement
 h1Elem.innerText = 'You will have 60 seconds to answer as many questions as you can. Good Luck!!'
-formReadyBut.addEventListener('click',function(){
-    countDown()
+formReadyBut.addEventListener('click', function () {
     formReadyBut.style.display = 'none'
     ansForm.style.display = 'block'
+    countDown()
     ready = true;
     questionsSet(ready);
 
@@ -104,7 +92,7 @@ formReadyBut.addEventListener('click',function(){
 formAns.addEventListener('click', function (event) {
     var index = event.target.id;
     index = parseInt(index);
-    playerAns.push(qAll[randomQ][index][1]);
+    
 
     if (qAll[randomQ][index][1]) {
         valid.innerText = 'Correct Answer';
@@ -126,80 +114,4 @@ formAns.addEventListener('click', function (event) {
 
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// setInterval variable to make timer and display in <p> in 
-// var playTime = setInterval(function () {
-//     counter--;
-//     console.log(counter)
-//     document.getElementById('countDown').innerHTML = 'TIME REMAINING: ' + counter;
-//     if (counter <= 0) {
-//         clearInterval(playTime)
-//         alert('Times is up! Let\'s checkout your score')
-//     }
-// }, 100);
-
-// if (ready) {
-
-//     randomQ = rando();
-
-//     //set button value equal to answers
-//     a1.value = qAll[randomQ][1][0];
-//     a2.value = qAll[randomQ][2][0];
-//     a3.value = qAll[randomQ][3][0];
-//     a4.value = qAll[randomQ][4][0];
-
-//     //set h1 innerText equal to question
-//     h1Elem.innerText = qAll[randomQ][0];
-
-// }
-
-
-// //event delegation event listener
-// formAns.addEventListener('click', function (event) {
-//     var index = event.target.id;
-//     index = parseInt(index);
-//     playerAns.push(qAll[randomQ][index][1]);
-
-//     if (qAll[randomQ][index][1]) {
-//         valid.innerText = 'Correct Answer';
-//         score++
-//         scoreDis.innerText = 'Score: ' + score;
-
-//     } else {
-//         counter = counter - 5;
-//         valid.innerText = 'Incorrect Answer => -5 Seconds';
-//     }
-
-
-//     randomQ = rando()
-//     h1Elem.innerText = qAll[randomQ][0];
-//     a1.value = qAll[randomQ][1][0];
-//     a2.value = qAll[randomQ][2][0];
-//     a3.value = qAll[randomQ][3][0];
-//     a4.value = qAll[randomQ][4][0];
-
-// })
 
